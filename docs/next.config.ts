5919ab4fa3cad9cc9386f20cf31fc7ae3c4208c2
lewwny <lenygarcia0302@gmail.com>
2026-04-10 14:31:41 +0200

    ok

--- docs/next.config.ts
+++ docs/next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  // Remplacez 'nom-du-repo' par le nom de votre projet GitHub
+  basePath: '/Hackathon-Open-data-University', 
+  // Permet de gérer les images correctement sans serveur d'optimisation
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
